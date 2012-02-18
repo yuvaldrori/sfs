2dfd62b10869963b57e4b5015dd0393b391d19cc
Yuval Drori <yuval.drori@gmail.com>
2012-02-18 15:26:51 +0200

    don't remember...

--- client.js
+++ client.js
@@ -1,4 +1,22 @@
 var sfu = function() {
+  var ZXING_MAXFILESIZE_DECODE = 2000000;
+/*  var findqr(data, theFile) {
+    console.log('find qr for ' + theFile.name);
+    if (theFile.size > ZXING_MAXFILESIZE_DECODE) {
+      console.log('calling newImage with resize for ' + theFile.name);
+      newImage(data, theFile, resizeImage);
+    } else {
+      console.log('calling newImage with send for ' + theFile.name);
+      newImage(data, theFile, sendImage);
+    }
+  };
+  var sendImage(image, theFile, cb) {
+    var fd = new FormData();
+    fd.append("full", "true");
+    fd.append("f", theFile);
+    var xhr = new XMLHttpRequest();
+    xhr.open  
+  };
   var resizeImage = function(image, theFile) {
     console.log('resize image ' + theFile.name);
     var ratio = 0.33;
@@ -10,14 +28,15 @@ var sfu = function() {
     canvas.height = image.height * ratio;
     context.drawImage(image, 0, 0, image.width * ratio, image.height * ratio);
   };
-  var newImage = function(data, theFile) {
+  var newImage = function(data, theFile, cb) {
     console.log('new image ' + theFile.name);
     var image = new Image();
     image.onload = function() {
-      resizeImage(image, theFile);
+      cb(image, theFile);
     }
     image.src = data;
   };
+ */ 
   return {
     handleFileSelect: function(e) {
                         var files = e.target.files;
@@ -31,7 +50,27 @@ var sfu = function() {
                           reader.onload = (function(theFile) {
                             return function(e) {
                               console.log('reader onload ' + theFile.name);
-                              newImage(e.target.result, theFile);
+                              if (theFile.size > ZXING_MAXFILESIZE_DECODE) {
+                                console.log('resize image ' + theFile.name);
+                              } else {
+                               console.log('no need to resize ' +
+                                theFile.name); 
+                               var fd = new FormData();
+                               fd.append('f', theFile);
+                               var xhr = new XMLHttpRequest();
+                               xhr.open('POST',
+                                 'http://zxing.org/w/decode.jspx', true);
+                               xhr.setRequestHeader("Access-Control-Allow-Origin",
+                                 "*");
+                               xhr.onload = function(e) {
+                                 if (xhr.status == 200) {
+                                   console.log('uploaded');
+                                 } else {
+                                   console.log('error uploading');
+                                 };
+                               };
+                               xhr.send(fd);
+                              };
                             };
                           })(f);
                           reader.readAsDataURL(f);
