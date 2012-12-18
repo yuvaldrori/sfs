922624abff6e503a250c1aff99b84955cb2774e8
Yuval Drori <yuval.drori@gmail.com>
2012-12-18 22:44:45 +0200

    checkpoint on the way to return list

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ var router = require('router');
 var route = router();
 var uuid = require('node-uuid');
 var static = require('node-static');
+var AWS = require('aws-sdk');
+
+AWS.config.update({region: 'us-east-1'});
 
 var sfsBucket=process.env.sfsBucket;
 
@@ -11,7 +14,7 @@ process.chdir(__dirname);
 
 route.get('/', function(req, res) {
   res.writeHead(200);
-  res.end('hello index page');
+  res.end('sfs');
 });
 
 var file = new(static.Server)('./static');
@@ -23,6 +26,32 @@ route.get('/event', function(req, res) {
   var eventName = uuid.v4();
   res.writeHead(200);
   res.end(sfsBucket + ':' + eventName);
+});
+
+route.post('/list', function(req, res) {
+
+  var data = '';
+  req.addListener('data', function(chunk) { data += chunk; });
+  req.addListener('end', function() {
+    params = JSON.parse(data);
+    if (params.Prefix === undefined || params.Prefix === '') {
+      res.writeHead(400);
+      res.end('will not list root');
+      return;
+    }
+    params.Bucket = sfsBucket;
+    var s3 = new AWS.S3();
+    s3.client.listObjects(params, function(err, data) {
+      console.log('2222');
+      if (err) {
+        res.writeHead(400);
+        res.end('aws error');
+        return;
+      }
+      res.writeHead(200);
+      res.end(util.inspect(data, true, null));
+    });
+  });
 });
 
 http.createServer(route).listen(80);
