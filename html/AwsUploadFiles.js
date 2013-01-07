60f80739977a659097dbc0ca5b4325df283e0939
Amit Feller <amit.feller@gmail.com>
2013-01-08 00:44:08 +0200

    uploading thumbnails pic

--- html/AwsUploadFiles.js
+++ html/AwsUploadFiles.js
@@ -45,9 +45,37 @@ AWSFiles.upload = function()
 			var fd = new FormData();
 			fd.append("key",folderName + "/${filename}");
 			fd.append("acl","public-read");
-			fd.append("Content-Type","image/jpeg'");
+			fd.append("Content-Type",file.type);
 			fd.append('file', file);
 			
+			var img = new Image();
+			img.src = $("img",el).attr('src');
+			img.onload = function()
+			{
+				alert('entered');
+				var minImageData =  resizeImage(img,0.5,file.type);
+				 
+				//decode the base64 binary into am ArrayBuffer 
+			    var separator = 'base64,';  
+				var index = minImageData.indexOf(separator);  
+				if (index != -1) {  
+	
+					var barray = Base64Binary.decodeArrayBuffer(minImageData.substring(index+separator.length)); 
+					var dv = new DataView(barray);
+					var blob = new Blob([dv],{type:file.type}); 
+
+					var fd2 = new FormData();
+					fd2.append("key",folderName + "/${filename}");
+					fd2.append("acl","public-read");
+					fd2.append("Content-Type",file.type);
+					fd2.append('file', blob, "thumb_"+file.name);
+					
+					var xhr = new XMLHttpRequest();
+					xhr.open("POST", url, true);
+					xhr.send(fd2);
+				}
+			}
+	  
 			sendForm(fd,AWSFiles.files[i].elem,url);
 		}
 	  }
@@ -84,7 +112,15 @@ function displayUploadProgress(el, event){
   };
   
   
-  		
+  function resizeImage(img,ratio,type)
+  {
+	 var canvas = document.createElement('canvas');
+     var context = canvas.getContext('2d');
+	 canvas.width = img.width * ratio;
+     canvas.height = img.height * ratio;
+	 context.drawImage(img, 0, 0, img.width * ratio, img.height * ratio);
+	 return canvas.toDataURL(type);
+  }
 		
   function preview(file,i)
   {
@@ -92,29 +128,29 @@ function displayUploadProgress(el, event){
 	var usePrevRow   = (i % 3);
 	var	previewRowNum  = Math.floor(i/3);
 	reader = new FileReader();
-	if(!usePrevRow)
+	if(usePrevRow == 0 )
 	{
-		$("#previewTable").append('<tr class="span3" id="previewRow'+previewRowNum+'"></tr>');
+		$("#previewTable").append('<tr id="previewRow'+previewRowNum+'"></tr>');
 	}
 	el = $(	'<td>'+
 				'<div class="thumbnail">' +
-						'<img class="img-rounded" width="150" height="150">' +
+						'<img class="img-rounded">' +
 					'<div class="caption">'+
 						'<p>' + file.name + '</p>'+
-						'<div class="progress progress-striped">'+
+						'<div class="progress progress-striped" visibility: hidden>'+
 							'<div class="bar" style="width: 0%;">'+
 							'</div>'+
 						'</div>'+
-						'<p>'+
-						'</p>'+
 					'</div>'+
 				'</div>'+
 			'</td>');
 	$("#previewRow"+previewRowNum).append(el);
-	img = $("img", el);
+	
+	var img = $("img", el);
 	reader.onload = function(e) {
+
 	  qrcode.decode(e.target.result);
-	  return img.attr('src', e.target.result);
+	  img.attr('src', e.target.result);
 	};
 	reader.readAsDataURL(file);
 	return el;
@@ -156,6 +192,7 @@ function sendForm(form,el,url)
 {
 	var xhr = new XMLHttpRequest();
 			
+	$(".progress", el).show();
 	xhr.upload.addEventListener("progress", (function(e) {
 	  return displayUploadProgress(el, e);
 	}), false);
@@ -165,6 +202,7 @@ function sendForm(form,el,url)
 		{
 		  if (xhr.status === 204) {
 			$(".progress", el).removeClass("active").addClass("progress-success");
+			$(".progress", el).hide();
 			return $(".caption p", el).text("Upload complete! ").append($("<a href=\"" + url + "\">View on S3</a>"));
 		  } else {
 			return $(".caption p", el).text("Upload failed ?");
