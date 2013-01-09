b7774ccda3de3d5c10351974e5a8c418c420ee5a
Yuval Drori <yuval.drori@gmail.com>
2013-01-09 17:32:39 +0200

    new event page

--- server/static/app/new.js
+++ server/static/app/new.js
@@ -1,3 +1,7 @@
+$(document).ready(handleGenQR());
+$("#website").html('<h3>Goto:</h3> <h2>' + window.location.origin +
+    '/app/upload.html')
+
 function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
   if ("withCredentials" in xhr) {
@@ -19,20 +23,28 @@ function handleGenQR()
 	var url = window.location.origin + '/event';
 	xhr = createCORSRequest('GET', url);
 
+  var qrTypeNumber = 5;
+  var qrErrorCorrectionLevel = 'M';
+  var size = Math.min($(window).width(), $(window).height()) * 0.7;
+  // cell size calc taken from genqr code
+  var qrCellSize = Math.floor(size / (qrTypeNumber * 4 + 25));
+
 	xhr.onload = function(e) {
 		if(xhr.readyState == 4 )
 		{
 			if (xhr.status == 200) {
-				  var qr = genQRcode(5,'M');
+				  var qr = genQRcode(qrTypeNumber,qrErrorCorrectionLevel);
 				  if(qr != null)
 				  {
 					qr.addData(xhr.responseText);
 					qr.make();
-					var img = qr.createImgTag();
+					var img = qr.createImgTag(qrCellSize);
 
 					if(img != null)
 					{
-						$('#genQrBtn').append(img);
+						$('#qr').append(img);
+            $("#qr").css('margin', '0 auto');
+            $("#qr").css('width', $("#qr>img").width());
 					}
   		  }
 			}
