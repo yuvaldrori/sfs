9b0e6e46ac4482fd922df52be725622df3cde15d
Yuval Drori <yuval.drori@gmail.com>
2013-02-26 23:23:39 +0200

    qr code for new event

--- server/static/app/main.js
+++ server/static/app/main.js
@@ -1,5 +1,32 @@
 $( document ).ready( function() {
   $( ".page" ).hide();
   $( "#welcome_page" ).show();
+  $( "#gencode_button" ).click(function() {
+    genQR();
+  });
 });
+
+function genQR() {
+	var url = window.location.origin + '/event';
+  var qrTypeNumber = 5;
+  var qrErrorCorrectionLevel = 'M';
+  var size = Math.min($(window).width(), $(window).height()) * 0.9;
+  // cell size calc taken from genqr code
+  var qrCellSize = Math.floor(size / (qrTypeNumber * 4 + 25));
+  $.ajax(url).done(function(text) {
+    var qr = genQRcode(qrTypeNumber,qrErrorCorrectionLevel);
+    if(qr != null) {
+      qr.addData(text);
+      qr.make();
+      var img = qr.createImgTag(qrCellSize);
+      $( "#folder>p" ).text(text);
+      if(img != null) {
+        $( "#qr_code" ).append(img);
+        $( "#website>p" ).text('goto: ' + window.location.origin);
+        $( ".page" ).hide();
+        $( "#new_event_page" ).show();
+      }
+    }
+  });
+}
 
