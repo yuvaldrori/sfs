5fa7c31e827a01c1c2e9740a8aa66dc61be8bef3
Yuval Drori <yuval.drori@gmail.com>
2013-03-01 17:41:20 +0200

    cropping on the way

--- server/static/app/main.js
+++ server/static/app/main.js
@@ -3,9 +3,13 @@ $( document ).ready( function() {
   $( "#gencode_button" ).click(function() {
     genQR();
   });
+  $( "#picture_button" ).click(function() {
+    pageTurn($( "#decode_qr_page" ));
+    $( "#magic_picture" ).change(decodeMagicPicture);
+  });
 });
 
-function pageTurn (page) {
+function pageTurn(page) {
   $( ".page" ).hide();
   $( page ).show();
 }
@@ -32,4 +36,55 @@ function genQR() {
     }
   });
 }
+
+function decodeMagicPicture() {
+  if (this.files && this.files.length > 0 && this.files[0].type.match('image.*')) {
+    var file = {};
+    var src = window.URL.createObjectURL(this.files[0]);
+    qrcode.callback = function(e, d) {
+      if (e) {
+        var jcrop_api;
+        var size = Math.min($(window).width(), $(window).height()) * 0.9;
+        $( "#bad_qr_image" ).load(function () {
+          file.width = this.width;
+          file.height = this.height;
+          $( "#bad_qr_image" ).width(size);
+          $( "#bad_qr_image" ).height(size);
+          jQuery(function($) {
+            $( "#bad_qr_image" ).Jcrop({
+              trueSize: [file.width, file.height],
+              setSelect: [file.width * 0.25,
+                          file.height * 0.25,
+                          file.width * 0.75,
+                          file.height * 0.75],
+              onChange: function(c) {
+                cropBox = c;
+              }
+            },
+            function() {
+              jcrop_api = this;
+            });
+          });
+          //window.URL.revokeObjectURL(src);
+        });
+        pageTurn($( "#crop_qr_page" ));
+        $( "#bad_qr_image" ).attr('src', src);
+        $( "#crop_button" ).click(function() {
+          c = jcrop_api.tellSelect();
+          //var canvas = document.createElement( "canvas" );
+          $( "#target_canvas" ).width(c.w);
+          $( "#target_canvas" ).height(c.h);
+          var context = $( "#target_canvas" )[0].getContext('2d');
+          context.drawImage($( "#bad_qr_image" )[0], c.x, c.y, c.w, c.h, 0, 0,
+            c.w, c.h);
+        });
+      } else {
+        window.URL.revokeObjectURL(src);
+        console.log(d);
+      };
+    };
+    qrcode.decode(src);
+  }
+}
+
 
