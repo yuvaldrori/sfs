2522f2dc87fa2d383a942ca41337dee93b15e093
Amit Feller <amit.feller@gmail.com>
2013-03-12 23:12:09 +0200

    adding pictures functionality it is not tested yet, but for the sake of  integration i am uploading it

---
+++ server/static/app/pictures.js
@@ -0,0 +1,326 @@
+
+
+/* Bind Functions */
+
+$('#files_uploader').bind('change', handleFileSelect);
+$('#downloadFiles').bind('change', handleDownloadFiles);
+
+$('#upload_pics"').bind('dragenter', handleDragEnter);
+$('#upload_pics"').bind('dragover', handleDragOver);
+$('#upload_pics"').bind('drop', handleFileDrop);
+
+$('#upload_button').bind('click', handleFileupload);
+
+/**************************************************/
+
+/* AWS Files Struct*/
+AWSFiles = {};
+AWSFiles.files = [];
+AWSFiles.bucketName = null;
+AWSFiles.upload = function()
+{
+	 for(var i =  0; i < AWSFiles.files.length ; i++)
+	 {
+		var file = AWSFiles.files[i].file;
+		var el = AWSFiles.files[i].elem;
+
+		if (file.size > (3 * 1024 * 1024)) 
+		{
+		  return $(".caption p", el).text("Sorry, file's too big!");
+		} 
+		else 
+		{
+			var bucketName = this.bucketName.split(":")[0];
+			var folderName = this.bucketName.split(":")[1];
+			var url = "https://"+bucketName+".s3.amazonaws.com/"
+			var fd = new FormData();
+			fd.append("key",folderName + "/${filename}");
+			fd.append("acl","public-read");
+			fd.append("Content-Type",file.type);
+			fd.append('file', file);
+			
+			var img = new Image();
+			img.src = $("img",el).attr('src');
+			img.onload = function()
+			{
+				alert('entered');
+				var minImageData =  resizeImage(img,0.5,file.type);
+				 
+				//decode the base64 binary into am ArrayBuffer 
+			    var separator = 'base64,';  
+				var index = minImageData.indexOf(separator);  
+				if (index != -1) {  
+	
+					var barray = Base64Binary.decodeArrayBuffer(minImageData.substring(index+separator.length)); 
+					var dv = new DataView(barray);
+					var blob = new Blob([dv],{type:file.type}); 
+
+					var fd2 = new FormData();
+					fd2.append("key",folderName + "/${filename}");
+					fd2.append("acl","public-read");
+					fd2.append("Content-Type",file.type);
+					fd2.append('file', blob, "thumb_"+file.name);
+					
+					var xhr = new XMLHttpRequest();
+					xhr.open("POST", url, true);
+					xhr.send(fd2);
+				}
+			}
+	  
+			sendForm(fd,AWSFiles.files[i].elem,url);
+		}
+	  }
+}
+
+
+
+AWSFiles.Init = function()
+{
+	AWSFiles.files = [];
+	AWSFiles.bucketName = null;
+}
+
+AWSFiles.readyToUpload = function()
+{
+	return AWSFiles.bucketName != null;
+}
+
+AWSFiles.addFile = function(f,elem){
+
+	var awsFile = new AWSFile(f,elem,true);
+	this.files.push(awsFile);
+	
+	//cancel pic function
+	$(elem).bind('click', function(el,aFile){
+		$(el).remove();
+		aFile.upload = false;
+	}(elem,awsFile)); 
+}
+
+
+function displayUploadProgress(el, event){
+    var percent;
+    if (event.lengthComputable) {
+      percent = Math.floor((event.loaded / event.total) * 100);
+      $(".progress", el).toggleClass("active", true);
+      $(".bar", el).css({
+        "width": "" + percent + "%"
+      });
+      return $(".caption p", el).text("" + percent + "% uploaded");
+    }
+  };
+  
+  
+  function resizeImage(img,ratio,type)
+  {
+	 var canvas = document.createElement('canvas');
+     var context = canvas.getContext('2d');
+	 canvas.width = img.width * ratio;
+     canvas.height = img.height * ratio;
+	 context.drawImage(img, 0, 0, img.width * ratio, img.height * ratio);
+	 return canvas.toDataURL(type);
+  }
+		
+		
+  function createPicPlaceHolder(file)
+  {
+	var el = $('<div class="pic_place_holder">' +
+				'<div class="title">'+
+					'<p>' + file.name +'('+file.size+' kb )'+ '</p>'+
+				'</div>'+
+			'</div>');
+	$('#previewImages').append(el);
+	return el;
+  }
+  
+  function preview(f)
+  {
+	var el, img, progress, reader;
+	
+	el = $('<div class="thumbnail">' +
+				'<img class="img-rounded"/>' +
+				'<div class="caption">'+
+					'<div class="progress" visibility: hidden/>'+
+				'<div/>'+
+			'</div>');
+	$(f.elem).append(el);
+	
+	var img = $("img", el);
+	reader.onload = function(e) {
+	  img.attr('src', e.target.result);
+	};
+	reader.readAsDataURL(f.file);
+  }
+  
+  
+  
+function handleDragEnter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function handleDragOver(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function handleFileDrop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  var dt = e.originalEvent.dataTransfer;
+  var files = dt.files;
+  handleFiles(files);
+}
+		
+function AWSFile (file,elem,upload) {
+    this.file = file;
+    this.elem = elem;
+	this.upload = upload;
+}
+
+function handleFiles(files)
+{
+	AWSFiles.Init();
+	
+	var file,elem;
+	$("#previewImages").children().remove();
+	for(var i=0; i < files.length ; i++)
+	{
+		file = files[i];
+		if (!file.type.match('image.*')) {
+			continue;
+		}
+		
+		elem = createPicPlaceHolder(file);
+		AWSFiles.addFile(file,elem);
+	}
+	
+	for(var i=0; i < AWSFiles.files.length ; i++)
+	{
+		preview(AWSFiles.files[i]);
+	}
+}
+	  
+  function handleFileSelect(event)
+  {
+	var files = event.target.files;
+	qrcode.callback = QRcallback;
+	handleFiles(files);
+  }
+	  
+  function handleFileupload()
+  {
+	AWSFiles.upload();
+  }
+	  
+  
+function sendForm(form,el,url)
+{
+	var xhr = new XMLHttpRequest();
+			
+	$(".progress", el).show();
+	xhr.upload.addEventListener("progress", (function(e) {
+	  return displayUploadProgress(el, e);
+	}), false);
+	
+	xhr.addEventListener("load", function(e) {
+		if(xhr.readyState == 4 )
+		{
+		  if (xhr.status === 204) {
+			$(".progress", el).removeClass("active").addClass("progress-success");
+			$(".progress", el).hide();
+			return $(".caption p", el).text("Upload complete! ").append($("<a href=\"" + url + "\">View on S3</a>"));
+		  } else {
+			return $(".caption p", el).text("Upload failed ?");
+		  }
+		}
+	},false);
+	
+	$(".caption p", el).text("Starting upload...");
+	xhr.open("POST", url, true);
+	xhr.send(form);
+}
+
+
+
+
+
+/************************************ Download Functions ****************************************************/
+
+function addImageThumbnail(fileName,awsBucketName)
+{
+	var td = document.createElement('td');
+	var name = document.createElement('b');
+	var url = "https://" + awsBucketName + ".s3.amazonaws.com/" + fileName;
+	var link = document.createElement('a');
+	var img = new Image(150,150);
+	link.href = url;
+	img.src = url;
+	img.class = "img-rounded";
+	img.onload = function() {
+		$('#downloadedPreview').append(td);
+	}
+	name.innerHTML = fileName.split("/")[1];
+	link.appendChild(img);
+	td.appendChild(link);
+	td.appendChild(name);
+	
+}
+
+function downloadFiles(JSONresponse)
+{
+	
+	var listObjects = JSON.parse(JSONresponse);
+	if(listObjects.Contents && listObjects.Name)
+	{
+		$(listObjects.Contents).each(function() {  
+			var fileName = this.Key;
+			addImageThumbnail(fileName,listObjects.Name);
+		});
+	}
+}
+function getFilesList(decodedQR)
+{
+	var bucketName = decodedQR.split(":")[0];
+	var folderName = decodedQR.split(":")[1];
+	
+	//TODO: replace this url!!
+	var url = "http://localhost:80/list";
+	var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+    // send the collected data as JSON
+	var data = {};
+	data["Prefix"] = folderName + '/';
+	data["Delimeter"] = "";
+
+    xhr.send(JSON.stringify(data));
+    xhr.onloadend = function () {
+      if(xhr.readyState == 4 )
+	  {
+		if (xhr.status == 200) {
+			downloadFiles(xhr.responseText);
+		}
+	  }
+						
+    };
+}
+
+ function handleDownloadFiles(e)
+  {
+	var file = event.target.files[0];
+	qrcode.callback = getFilesList;
+	if (!file.type.match('image.*')) {
+		alert(file.name + ' is not an image!');
+	}
+	var reader = new FileReader();
+	reader.onload = function(e) {
+	  qrcode.decode(e.target.result);
+	};
+	reader.readAsDataURL(file);
+  }
+  
+	
+	
